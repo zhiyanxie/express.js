b6562933101b3d3a3d6aa976af5190fd240027f5
Zhiyan Xie <xiezhiyanhfut@hotmail.com>
2016-07-25 13:59:38 -0500

    make function that pass nav as parameter

--- app.js
+++ app.js
@@ -4,13 +4,29 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
 
-var bookRouter = express.Router();
+var nav = [{
+    Link: '/Books',
+    Text: 'Book'
+}, {
+    Link: '/Authors',
+    Text: 'Author'
+}];
+
+var bookRouter = require('./src/routes/bookRoutes')(nav);
+
+
 
 
 //middle ware static files first thing
 app.use(express.static('public'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+
+
+
+
+
+app.use('/Books', bookRouter);
 
 app.get('/', function(req, res) {
     res.render('index', {
