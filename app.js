9c6b5c4bc7574caab1db806a5595530da4e2aab5
Zhiyan Xie <xiezhiyanhfut@hotmail.com>
2016-07-24 23:02:31 -0500

    add ejs for templating enging

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static('src/views'));
 app.get('/', function(req, res){
   res.send('hello world');
 });
-var port = 5000;
+var port = process.env.PORT || 5000;
 app.listen(5000, function(err){
   console.log('running server on port ' + port);
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,56 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var nodemon = require('nodemon');
 
 var jsFiles = ['*.js', 'src/**/*.js'];
-gulp.task('style', function(){
-  return gulp.src(jsFiles)
-      .pipe(jshint())
-      .pipe(jshint.reporter('jshint-stylish', {
-        verbose: true
-      }))
-      .pipe(jscs())
-      .pipe(jscs.reporter());
+gulp.task('style', function() {
+    return gulp.src(jsFiles)
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish', {
+            verbose: true
+        }))
+        .pipe(jscs())
+        .pipe(jscs.reporter());
 });
 
 
-gulp.task('inject', function(){
-  var wiredep = require('wiredep').stream;
-  var inject = require('gulp-inject');
-  var injectSrc = gulp.src(['./public/css/*.css',
-    './public/js/*.js'],
-    { read: false});//just need the name
+gulp.task('inject', function() {
+    var wiredep = require('wiredep').stream;
+    var inject = require('gulp-inject');
+    var injectSrc = gulp.src(['./public/css/*.css',
+        './public/js/*.js'
+    ], {
+        read: false
+    }); //just need the name
 
-  var injectOptions = {
-    ignorePath: '/public'
-  };
-  var options = {
-    bowerJson: require('./bower.json'),
-    directory: './public/lib',
-    ignorePath: '../../public'
-  }
+    var injectOptions = {
+        ignorePath: '/public'
+    };
+    var options = {
+        bowerJson: require('./bower.json'),
+        directory: './public/lib',
+        ignorePath: '../../public'
+    }
 
-  return gulp.src('./src/views/*.html')
-    .pipe(wiredep(options))
-    .pipe(inject(injectSrc, injectOptions))
-    .pipe(gulp.dest('./src/views'));
+    return gulp.src('./src/views/*.html')
+        .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
+        .pipe(gulp.dest('./src/views'));
 
+});
+
+gulp.task('serve', ['style', 'inject'], function() {
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        env: {
+            'PORT': 5000
+        },
+        watch: jsFiles
+    }
+    return nodemon(options)
+        .on('restart', function(ev) {
+            console.log('Restarting');
+        })
 });
