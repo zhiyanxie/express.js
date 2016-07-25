8996e52b14a05ab5052d4c6bd3b3a520fedf434b
Zhiyan Xie <xiezhiyanhfut@hotmail.com>
2016-07-24 22:07:48 -0500

    edit gulpfile gulp-inject

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,14 @@ gulp.task('style', function(){
 
 gulp.task('inject', function(){
   var wiredep = require('wiredep').stream;
+  var inject = require('gulp-inject');
+  var injectSrc = gulp.src(['./public/css/*.css',
+    './public/js/*.js'],
+    { read: false});//just need the name
+
+  var injectOptions = {
+    ignorePath: '/public'
+  };
   var options = {
     bowerJson: require('./bower.json'),
     directory: './public/lib',
@@ -24,6 +32,7 @@ gulp.task('inject', function(){
 
   return gulp.src('./src/views/*.html')
     .pipe(wiredep(options))
+    .pipe(inject(injectSrc, injectOptions))
     .pipe(gulp.dest('./src/views'));
 
 });
