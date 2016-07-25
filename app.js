291a2f0730ab47bc9221e0f881461509d3f4d0bb
Zhiyan Xie <xiezhiyanhfut@hotmail.com>
2016-07-24 23:23:42 -0500

    set up view engine with ejs and edit index.ejs

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ var app = express();
 
 //middle ware static files first thing
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-  res.send('hello world');
+  res.render('index', {title: 'hello from render', list:['a', 'b']});
 });
 var port = process.env.PORT || 5000;
 app.listen(5000, function(err){
